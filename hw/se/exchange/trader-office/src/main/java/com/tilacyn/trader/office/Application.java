5f0d65d985d69f43ae6d5cfdaec5b58a84e3e83c
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-04-19 20:00:15 +0300

    added se tasks

--- hw/se/exchange/trader-office/src/main/java/com/tilacyn/trader/office/Application.java
+++ hw/se/exchange/trader-office/src/main/java/com/tilacyn/trader/office/Application.java
@@ -1,14 +1,8 @@
 package com.tilacyn.trader.office;
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Collections;
-
-@SpringBootApplication
 public class Application {
-        public static void main(String[] args) {
-            SpringApplication.run(Application.class, args);
-        }
+    public static void main(String[] args) {
+
+    }
 }
--- hw/se/exchange/trader-office/src/test/java/com/tilacyn/trader/office/service/TradeOfficeServiceTest.java
+++ hw/se/exchange/trader-office/src/test/java/com/tilacyn/trader/office/service/TradeOfficeServiceTest.java
@@ -1,19 +1,31 @@
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
+package com.tilacyn.trader.office.service;
+
+
+import com.tilacyn.trader.office.rest.EmulatorRestService;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
+import org.testcontainers.containers.GenericContainer;
 
 import static org.junit.Assert.*;
 
-@RunWith(Arquillian.class)
-public class TradeOfficeServiceTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(com.tilacyn.trader.office.service.TradeOfficeService.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
 
+public class TradeOfficeServiceTest {
+    private TradeOfficeService service = new TradeOfficeService(new EmulatorRestService(8081));
+
+
+    @ClassRule
+    public static GenericContainer simpleWebServer
+            = new FixedHostPortGenericContainer("emulator:1.0-SNAPSHOT")
+            .withFixedExposedPort(8081, 8080);
+
+
+    @Test
+    public void testQuote() {
+        assertTrue(service.getQuote("AAPL") < 270);
+        assertTrue(service.getQuote("AAPL") > 230);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
 }
